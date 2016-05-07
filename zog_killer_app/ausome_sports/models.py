b655c33a6bb28c23210fd8a62ac8f691b269279c
Andrew Wolfram <ajwolfram@gmail.com>
2016-05-07 18:09:35 -0400

    Create first draft set of data models

--- zog_killer_app/ausome_sports/models.py
+++ zog_killer_app/ausome_sports/models.py
@@ -3,5 +3,124 @@ from django.db import models
 # Create your models here.
 
 class User(models.Model):
-    pass
 
+    first_name = models.CharField(max_length=100) 
+    last_name = models.CharField(max_length=100) 
+    date_of_birth = models.DateField() 
+    sex = models.CharField(max_length=6, choices=[('M', 'Male'), ('W', 'Female'),]) 
+    e-mail = models.CharField(max_length=100) 
+    password = models.CharField(max_length=100) 
+    leagues = 
+    teams = 
+    pending_teams = 
+    active = models.BooleanField() 
+    picture = models.CharField(max_length=100)  
+    visible_in_directory = models.BooleanField()
+    date_added = models.DateTimeField(auto_now_add=True) 
+
+    def natural_key(self):
+        pass
+
+    def __repr__(self):
+        pass
+
+class League(models.Model):
+
+    name = models.TextField()
+    sport = models.CharField(max_length=20) 
+    city = models.CharField(max_length=100) 
+    state = models.CharField(max_length=100) 
+    country = models.CharField(max_length=2) 
+    start_date = models.DateField() 
+    end_date = models.DateField() 
+    description = models.TextField() 
+    difficulty = models.CharField(max_length=20) 
+    status = models.CharField(max_length=20) 
+    date_added = models.DateTimeField(auto_now_add=True) 
+    
+    def natural_key(self):
+        pass
+
+    def __repr__(self):
+        pass
+
+
+class Team(models.Model):
+
+    name = models.CharField(max_length=50) 
+    league = models.ForeignKey(League, on_delete=models.CASCADE) 
+    creator = models.ManyToManyField(User, null=True, blank=True) 
+    team_type = models.CharField(max_length=15, choices=[('R', 'Random'), ('U', 'User Generated'),]) 
+    open_call = models.BooleanField(default=False) 
+    date_added = models.DateTimeField(auto_now_add=True) 
+
+    def natural_key(self):
+        pass
+
+    def __repr__(self):
+        pass
+
+class TeamMember(models.Model):
+
+    team = models.ForeignKey(Team, on_delete=models.CASCADE) 
+    user = models.ForeignKey(User, on_delete=models.CASCADE) 
+    date_added = models.DateTimeField(auto_now_add=True) 
+    captain = models.BooleanField(default=False) 
+
+    def natural_key(self):
+        pass
+
+    def __repr__(self):
+        pass
+
+class PendingTeamMember(models.Model):
+
+    team = models.ForeignKey(Team, on_delete=models.CASCADE) 
+    user = models.ForeignKey(User, on_delete=models.CASCADE) 
+    date_added = models.DateTimeField(auto_now_add=True) 
+
+    def natural_key(self):
+        pass
+
+    def __repr__(self):
+        pass
+
+class Game(models.Model):
+
+    league = models.ForeignKey(League, on_delete=models.CASCADE) 
+    teams = models.ManyToManyField(Team, null=True, blank=True) 
+    date = models.DateTimeField() 
+    location = models.TextField() 
+    date_added = models.DateTimeField(auto_now_add=True) 
+
+    def natural_key(self):
+        pass
+
+    def __repr__(self):
+        pass
+
+class Win(models.Model):
+
+    team = models.ForeignKey(Team, on_delete=models.CASCADE, null=True, blank=True) 
+    game = models.ForeignKey(Game, on_delete=models.CASCADE) 
+    score = models.CharField(max_length=50, null=True, blank=True) 
+    date_added = models.DateTimeField(auto_now_add=True) 
+
+    def natural_key(self):
+        pass
+
+    def __repr__(self):
+        pass
+
+class Loss(models.Model):
+
+    team = models.ForeignKey(Team, on_delete=models.CASCADE, null=True, blank=True) 
+    game = models.ForeignKey(Game, on_delete=models.CASCADE) 
+    score = models.CharField(max_length=50, null=True, blank=True) 
+    date_added = models.DateTimeField(auto_now_add=True) 
+
+    def natural_key(self):
+        pass
+
+    def __repr__(self):
+        pass
