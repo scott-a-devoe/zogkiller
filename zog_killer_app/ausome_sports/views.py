22362ef2412ba40a45afc7f240780d55a7aae7b7
Andrew Wolfram <ajwolfram@gmail.com>
2016-05-08 00:15:41 -0400

    Update first view for retrieving current user profile

--- zog_killer_app/ausome_sports/views.py
+++ zog_killer_app/ausome_sports/views.py
@@ -1,5 +1,15 @@
+from django.core import serializers
+from django.http import JsonResponse
 from django.shortcuts import render
+from . import models, decorators
 
 # Create your views here.
 def index(request):
     return render(request, 'ausome_sports/index.html')
+
+@decorators.login_required
+def get_user_profile(request):
+    profile_set = models.AusomeUser.objects.filter(user=request.user)
+    #data = serializers.serialize('json', profile_set, fields=('first_name', 'last_name'))
+
+    return JsonResponse(data=profile_set, safe=False)
