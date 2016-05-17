c3bef7b5b7272435371b7170bbe0a440a23204bf
Andrew Wolfram <ajwolfram@gmail.com>
2016-05-17 19:38:50 -0400

    Create util for commonly used functions

---
+++ zog_killer_app/ausome_sports/utils.py
@@ -0,0 +1,69 @@
+from django.contrib.auth.models import User 
+from django.core.urlresolvers import reverse
+from django.utils import dateparse
+
+from datetime import date, datetime
+from . import models
+
+def verify_login_required(self, view_name, args=None):
+    response = self.client.get(reverse(view_name, args=args))
+    self.assertEqual(response.status_code, 403)
+
+    self.client.post(reverse('post_login'), 
+            {'username': 'testuser', 'password': 'password99'}
+            )
+
+    response = self.client.get(reverse(view_name, args=args))
+    self.assertEqual(response.status_code, 200)
+    return response
+
+
+def create_ausome_user():
+    auth_user = User.objects.create_user(username='testuser',
+            email='test@test.com',
+            password='password99',
+            )
+    ausome_user = models.AusomeUser()
+    ausome_user.user = auth_user
+    ausome_user.email = auth_user.email
+    ausome_user.first_name = 'First'
+    ausome_user.last_name = 'Last'
+    ausome_user.dob = dateparse.parse_date('1990-01-02')
+    ausome_user.sex = 'male'
+    ausome_user.picture = 'example.jpg'
+    ausome_user.phone = '12334556788'
+    ausome_user.bio = 'Spike it real good!'
+    ausome_user.visible_in_directory = True
+    ausome_user.save()
+
+    return ausome_user
+
+def create_ausome_league():
+    league = models.League() 
+    league.name = 'Beach Volleyball'
+    league.sport = 'volleyball'
+    league.city = 'Austin'
+    league.state = 'TX'
+    league.country = 'US'
+    league.start_date = dateparse.parse_date('2016-06-15') 
+    league.end_date = dateparse.parse_date('2016-08-15') 
+    league.description = 'Advanced beach volleyball played Wednesday nights at Zilker park'
+    league.difficulty = 'advanced'
+    league.status = 'open'
+    league.team_max = 10
+    league.save()
+
+    return league
+    
+def create_ausome_team(league=None, creator=None):
+    team = models.Team()
+    team.name = 'Awaiting assignment'
+    team.league = league
+    team.creator = creator
+    team.team_type = 'R'
+    team.player_max = 10 
+    team.open_registration = False
+    team.save()
+
+    return team
+    
